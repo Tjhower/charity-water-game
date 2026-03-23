49146f867b9b3d2e2368405bb5114fe4be1ebb64
TJ Hower <tjhower2004@tamu.edu>
2026-03-23 13:46:43 +0000

    commit

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
-const canvas = document.getElementById("gameCanvas");
+document.addEventListener("DOMContentLoaded", function () {
+  // DOM elements
+  // const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
+
+//Load Title
+const titleImage = document.getElementById("titleImage");
+
+let titleReady = false;
+titleImage.onload = () => {
+  console.log("Image loaded!");
+  titleReady = true;
+};
+
+titleImage.onerror = () => {
+  console.error("Image failed to load!");
+};
+
+
+
+//Play Button Setup
+const playButton = {
+  x:860,
+  y:600,
+  
+}
+
 
 // Start game on click
 canvas.addEventListener("click", () => {
@@ -29,10 +54,20 @@ function gameLoop(){
 }
 
 function drawTitle(){
-  ctx.fillStyle = "white";
-  ctx.font = "24px monospace";
-  ctx.fillText("Wally the Wombat", 180, 200);
+    if(titleImage.complete){
+  ctx.drawImage(titleImage,760, 200, 400, 200);
+    }
+    let titleReady = false;
+    const titleImage = new Image();
+    titleImage.src = "img/wombat-well-builder-title.svg";
+
+    titleImage.onload = () => {
+         titleReady = true;
+        };
+          
   ctx.fillText("Click to Start", 220, 250);
+
+  
 }
 
 function drawMinigame(){
